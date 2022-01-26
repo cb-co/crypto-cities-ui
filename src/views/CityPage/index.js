b9642922115aabd60bc307614436e0952c2374c0
Robert <cmcorporations.adm@gmail.com>
2022-01-26 19:19:54 -0400

    Create transfer function

--- src/views/CityPage/index.js
+++ src/views/CityPage/index.js
@@ -10,7 +10,9 @@ import {
   Tbody,
   Button,
   Tag,
+  useToast,
 } from '@chakra-ui/react';
+import { useState } from 'react';
 import { useWeb3React } from '@web3-react/core';
 import { useParams } from 'react-router-dom';
 
@@ -18,11 +20,62 @@ import { useCryptoCityData } from '../../hooks/useCryptoCitiesData';
 import RequestAccess from '../../components/RequestAccess';
 import CityCard from '../../components/CityCard';
 import Loading from '../../components/Loading';
+import useCryptoCities from '../../hooks/useCryptoCities';
 
 const CityPage = () => {
-  const { active, account } = useWeb3React();
+  const { active, account, library } = useWeb3React();
   const { tokenId } = useParams();
-  const { loading, city } = useCryptoCityData(tokenId);
+  const { loading, city, update } = useCryptoCityData(tokenId);
+  const cryptoCities = useCryptoCities();
+  const toast = useToast();
+  const [transferring, setTransferring] = useState(false);
+
+  const transfer = () => {
+    setTransferring(true);
+
+    const address = prompt('Enter the address: ');
+    const isAddress = library.utils.isAddress(address);
+
+    if (!isAddress) {
+      toast({
+        title: 'Invalid address',
+        description: 'That is not a valid ethereum address.',
+        status: 'error',
+      });
+      setTransferring(false);
+    } else {
+      cryptoCities.methods
+        .safeTransferFrom(city.owner, address, city.tokenId)
+        .send({
+          from: account,
+        })
+        .on('error', (error) => {
+          setTransferring(false);
+          toast({
+            title: 'Transaction failed',
+            description: error.message,
+            status: 'error',
+          });
+        })
+        .on('transactionHash', (txHash) => {
+          toast({
+            title: 'Transaction sent',
+            description: txHash,
+            status: 'info',
+          });
+        })
+        .on('receipt', () => {
+          setTransferring(false);
+          toast({
+            title: 'Transaction confirmed',
+            description: `The city now belongs to ${address}`,
+            status: 'success',
+          });
+
+          update();
+        });
+    }
+  };
 
   if (!active) return <RequestAccess />;
 
@@ -45,7 +98,12 @@ const CityPage = () => {
             city.image.split('ipfs://')[1]
           }`}
         />
-        <Button disabled={account !== city.owner} colorScheme='orange'>
+        <Button
+          onClick={transfer}
+          isLoading={transferring}
+          disabled={account !== city.owner}
+          colorScheme='orange'
+        >
           {account !== city.owner ? 'You are not the owner' : 'Transfer'}
         </Button>
       </Stack>
